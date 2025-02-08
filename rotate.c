029c38c91d9067bc6d649a08c0d8292a3456580e
Oussama Ait H'Mad <oait-h-m@e2r9p12.1337.ma>
2025-02-08 18:16:23 +0100

    These are some functions that shift up all elements of stack A by 1. The first element becomes the last one.

---
+++ rotate.c
@@ -0,0 +1,49 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   rotate.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: oait-h-m <oait-h-m@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/08 15:15:58 by oait-h-m          #+#    #+#             */
+/*   Updated: 2025/02/08 15:18:34 by oait-h-m         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include "push_swap.h"
+
+void	rotate(t_list **head)
+{
+	t_list	*node;
+	t_list	*last;
+	t_list	*second_node;
+
+	node = (*head);
+	last = find_last_node(node);
+	second_node = (*head)->next;
+	(*head)->next = NULL;
+	last->next = *head;
+	*head = second_node;
+}
+
+void	ra(t_list **stack_a)
+{
+	if (*stack_a == NULL || (*stack_a)->next == NULL)
+		return ;
+	rotate(stack_a);
+	write(1, "ra\n", 3);
+}
+
+void	rb(t_list **stack_b)
+{
+	if (*stack_b == NULL || (*stack_b)->next == NULL)
+		return ;
+	rotate(stack_b);
+	write(1, "rb\n", 3);
+}
+
+void	rr(t_list **stack_a, t_list **stack_b)
+{
+	ra(stack_a);
+	rb(stack_b);
+	write(1, "rr\n", 3);
+}
