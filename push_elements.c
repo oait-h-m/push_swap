d0e073d599d7a46a8307ef260e6f16c5a9bc1b12
Oussama Ait H'Mad <oait-h-m@e2r9p12.1337.ma>
2025-02-08 18:12:25 +0100

    These are some functions that helped me push an element onto a stack.

---
+++ push_elements.c
@@ -0,0 +1,52 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_elements.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: oait-h-m <oait-h-m@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/08 15:14:42 by oait-h-m          #+#    #+#             */
+/*   Updated: 2025/02/08 15:15:42 by oait-h-m         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+void	push(t_list **stack_a, t_list **stack_b)
+{
+	t_list	*second;
+	t_list	*first;
+
+	if (*stack_a == NULL)
+		return ;
+	if (*stack_b == NULL)
+	{
+		*stack_b = *stack_a;
+		*stack_a = (*stack_b)->next;
+		(*stack_b)->next = NULL;
+	}
+	else
+	{
+		second = (*stack_a)->next;
+		first = *stack_a;
+		(*stack_a)->next = *stack_b;
+		*stack_a = second;
+		*stack_b = first;
+	}
+}
+
+void	pa(t_list **stack_a, t_list **stack_b)
+{
+	if (*stack_b == NULL)
+		return ;
+	push(stack_b, stack_a);
+	write(1, "pa\n", 3);
+}
+
+void	pb(t_list **stack_a, t_list **stack_b)
+{
+	if (*stack_a == NULL)
+		return ;
+	push(stack_a, stack_b);
+	write(1, "pb\n", 3);
+}
