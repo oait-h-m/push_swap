2c91857d3120150c92817f90d3459a8aedf46841
Oussama Ait H'Mad <oait-h-m@e1r6p11.1337.ma>
2025-02-17 01:13:48 +0100

    add some prototype functions

--- push_swap.h
+++ push_swap.h
@@ -6,7 +6,7 @@
 /*   By: oait-h-m <oait-h-m@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/08 15:41:13 by oait-h-m          #+#    #+#             */
-/*   Updated: 2025/02/08 15:41:17 by oait-h-m         ###   ########.fr       */
+/*   Updated: 2025/02/09 17:08:15 by oait-h-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,17 +22,19 @@
 typedef struct s_list
 {
 	int				value;
+	int				index;
 	struct s_list	*next;
 }					t_list;
 
 char				**ft_split(char const *s, char c);
+void				freesp(char **s);
 int					ft_atoi(const char *str);
-int					check_argu(char *arg);
 t_list				*create_node(int value);
 t_list				*find_before_last_node(t_list *head);
 t_list				*find_last_node(t_list *head);
 t_list				*sa(t_list **stack_a);
 t_list				*sb(t_list **stack_a);
+t_list				*min_value(t_list *head);
 void				ss(t_list **stack_a, t_list **stack_b);
 void				push(t_list **stack_a, t_list **stack_b);
 void				pb(t_list **stack_a, t_list **stack_b);
@@ -46,8 +48,12 @@ void				rra(t_list **stack_a);
 void				rrb(t_list **stack_b);
 void				rrr(t_list **stack_a, t_list **stack_b);
 void				add_to_last(t_list **head, int value);
-
+void				sort_three(t_list **stack_a);
+void				sort_four_elements(t_list **stack_a, t_list **stack_b);
+void				sort_five_elements(t_list **stack_a, t_list **stack_b);
+void				find_and_pb(t_list **stack_a, t_list **stack_b);
 int					check_error(char **av);
 int					repetetion(t_list *a, int nbr);
+int					count_list(t_list *head);
 
 #endif
