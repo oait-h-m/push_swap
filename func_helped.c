f994bd71220f9ca261dce90770123533dc6da59a
Oussama Ait H'Mad <oait-h-m@e1r6p9.1337.ma>
2025-02-18 00:47:27 +0100

    update and add functions.

--- func_helped.c
+++ func_helped.c
@@ -14,7 +14,7 @@
 
 int	count_list(t_list *head)
 {
-	t_list *lst;
+	t_list	*lst;
 	int		counter;
 
 	lst = head;
@@ -24,19 +24,19 @@ int	count_list(t_list *head)
 		counter++;
 		lst = lst->next;
 	}
-	return counter;
+	return (counter);
 }
 
 t_list	*min_value(t_list *head)
 {
-	t_list *lst;
-	t_list *min; 
-	int	i, (min_index);
+	t_list	*lst;
+	t_list	*min;
 
+	int i, (min_index);
 	if (!head)
-		return (NULL); 
-	min = head; 
-	lst = head; 	
+		return (NULL);
+	min = head;
+	lst = head;
 	i = 0;
 	min_index = 0;
 	while (lst)
@@ -45,17 +45,17 @@ t_list	*min_value(t_list *head)
 		if (lst->value < min->value)
 		{
 			min = lst;
-			min_index = i; 
+			min_index = i;
 		}
 		lst = lst->next;
 	}
-	min->index = min_index; 
+	min->index = min_index;
 	return (min);
 }
 
 void	find_and_pb(t_list **stack_a, t_list **stack_b)
 {
-	t_list *min;
+	t_list	*min;
 
 	min = min_value(*stack_a);
 	if (min->index > 2)
@@ -74,3 +74,43 @@ void	find_and_pb(t_list **stack_a, t_list **stack_b)
 	}
 }
 
+void	bubble_sort(int *arr, int size)
+{
+	int i, (j), (tmp);
+	i = 0;
+	while (i < size)
+	{
+		j = 0;
+		while (j < size - 1)
+		{
+			if (arr[j] > arr[j + 1])
+			{
+				tmp = arr[j];
+				arr[j] = arr[j + 1];
+				arr[j + 1] = tmp;
+			}
+			j++;
+		}
+		i++;
+	}
+}
+
+int	*sorted_array(t_list *stack_a)
+{
+	t_list	*tmp;
+	int		size;
+	int		*arr;
+
+	tmp = stack_a;
+	size = count_list(stack_a);
+	arr = (int *)malloc(sizeof(int) * size);
+	size = 0;
+	while (tmp)
+	{
+		arr[size] = tmp->value;
+		tmp = tmp->next;
+		size++;
+	}
+	bubble_sort(arr, size);
+	return (arr);
+}
