ee167006b2e0b66da650a69409b3036a06ca9b60
Oussama Ait H'Mad <oait-h-m@e1r6p8.1337.ma>
2025-02-20 19:32:35 +0100

    update

--- reverse_rotate.c
+++ reverse_rotate.c
@@ -26,21 +26,24 @@ void	reverse_rotate(t_list **head)
 	*head = last;
 }
 
-void	rra(t_list **stack_a)
+void	rra(t_list **stack_a, int a)
 {
 	reverse_rotate(stack_a);
-	write(1, "rra\n", 4);
+	if (a)
+		write(1, "rra\n", 4);
 }
 
-void	rrb(t_list **stack_b)
+void	rrb(t_list **stack_b, int a)
 {
 	reverse_rotate(stack_b);
-	write(1, "rrb\n", 4);
+	if (a)
+		write(1, "rrb\n", 4);
 }
 
-void	rrr(t_list **stack_a, t_list **stack_b)
+void	rrr(t_list **stack_a, t_list **stack_b, int a)
 {
-	rra(stack_a);
-	rrb(stack_b);
-	write(1, "rrr\n", 4);
+	rra(stack_a, a);
+	rrb(stack_b, a);
+	if (a)
+		write(1, "rrr\n", 4);
 }
