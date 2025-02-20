bb3a04c4aba754d4510fcb3ae543fb1cfe2d8f3c
Oussama Ait H'Mad <oait-h-m@e1r6p8.1337.ma>
2025-02-20 19:33:01 +0100

    update

--- rotate.c
+++ rotate.c
@@ -25,25 +25,28 @@ void	rotate(t_list **head)
 	*head = second_node;
 }
 
-void	ra(t_list **stack_a)
+void	ra(t_list **stack_a, int a)
 {
 	if (*stack_a == NULL || (*stack_a)->next == NULL)
 		return ;
 	rotate(stack_a);
-	write(1, "ra\n", 3);
+	if (a)
+		write(1, "ra\n", 3);
 }
 
-void	rb(t_list **stack_b)
+void	rb(t_list **stack_b, int a)
 {
 	if (*stack_b == NULL || (*stack_b)->next == NULL)
 		return ;
 	rotate(stack_b);
-	write(1, "rb\n", 3);
+	if (a)
+		write(1, "rb\n", 3);
 }
 
-void	rr(t_list **stack_a, t_list **stack_b)
+void	rr(t_list **stack_a, t_list **stack_b, int a)
 {
-	ra(stack_a);
-	rb(stack_b);
-	write(1, "rr\n", 3);
+	ra(stack_a, a);
+	rb(stack_b, a);
+	if (a)
+		write(1, "rr\n", 3);
 }
